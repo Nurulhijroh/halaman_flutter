78b51f7e0ff4896f2fe2c72c142a0fd8401f43d0
Nurulhijroh <nuyungbaik@gmail.com>
2025-05-12 13:33:50 +0700

    perbaiki eror

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'home_page.dart';
+import 'second_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,20 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: Color.white,
-        fontFamily: 'Roboto',
-        textTheme: const TextTheme(
-          bodyLarge: TextStyle(fontSize: 18, color: Color.black87),
-          titleLarge: TextStyle(fontSize: 22, fontWeight: fontWeight.bold),
-        ),
-        appBarTheme: const appBarTheme(
-          color: Color.blue,
-          titleTextStyle: TextStyle(fontSize: 20, fontWeight: fontWeight.bold),
-        ),
-      ),
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
+     
       home: const HomePage(),
     );
      
@@ -44,7 +35,7 @@ class HomePage extends StatelessWidget {
       child: ElevatedButton(
         child: const Text('ke Halaman kedua'),
         onPressed: (){
-          Navigator.Push(
+          Navigator.push(
           context,
           MaterialPageRoute(builder: (context) => const SecondPage()),
           );
